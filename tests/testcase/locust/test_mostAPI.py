b6682c4fd43c8f1ce1d9b94467e579ae2ffbd980
yibo.feng <yibo.feng@thoughtworks.com>
2020-01-07 17:51:12 +0800

    添加测试多接口

---
+++ tests/testcase/locust/test_mostAPI.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -
+from locust import HttpLocust, task
+from common.runMain import RunLocust
+from common.readConfig import *
+from tests.api import mockServerConfigsInfo, mockServerMockShowLists
+
+
+class TestLocust(RunLocust):
+
+    def on_start(self):
+        print('开始性能测试')
+
+    @task
+    def test_demo_post(self):
+        page_num, num = 10, 1
+        self.runLocust(mockServerMockShowLists.mock_show_lists(page_num, num))
+        self.runLocust(mockServerConfigsInfo.configs_info())
+
+
+class Query(HttpLocust):
+    task_set = TestLocust
+    min_wait = 1000
+    max_wait = 3000
+    host = host
